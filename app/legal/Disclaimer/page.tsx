afd8c8d9c3826fd9c9537f030e3e533174d214b5
Roberto Carlos de Oliveira Junior <rcoj@Air-de-Roberto.home>
2026-01-25 22:25:34 +0100

    fix: add export default to disclaimer page

--- app/legal/Disclaimer/page.tsx
+++ app/legal/Disclaimer/page.tsx
@@ -2,17 +2,14 @@ import { Header } from "@/components/header"
 import { SiteFooter } from "@/components/site-footer"
 import { DisclaimerSection } from "@/components/disclaimer-section"
 
-// IMPORTANTE: Tem que ter "export default" para a página carregar
+// O segredo está aqui: tem que ser "export default"
 export default function DisclaimerPage() {
   return (
     <div className="min-h-screen flex flex-col bg-white">
       <Header />
-      
-      {/* Aqui carregamos o componente visual que você criou */}
       <main className="flex-1">
         <DisclaimerSection />
       </main>
-
       <SiteFooter />
     </div>
   )
